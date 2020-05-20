b572c246d84fbed473e4fc2e85ffac945a8efb0f
Anurag <anurag.khandelwal@primussoft.com>
2020-05-20 19:15:47 +0530

    day 2 exercises soleved

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,21 @@
+
+// Don't change the export values.
+// You can use the function in the Math module
+
+function sqrt(number) {
+    return Math.sqrt(number);
+}
+
+function power(number1, number2) {
+    return Math.pow(number1, number2);
+}
+
+function round(number) {
+    return Math.round(number);
+}
+
+export {
+    sqrt,
+    power,
+    round,
+};
